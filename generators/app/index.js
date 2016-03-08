0d475b1d53d2db010af99001e612b04b1bfea765
Evan Duncan <evan.e.duncan@gmail.com>
2016-03-07 20:00:17 -0600

    feat(app) Start application generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,15 +9,39 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the ' + chalk.red('mean2') + ' generator!'
+      'Welcome to the ' + chalk.cyan('mean2') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What would you like to name your application?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'How would you describe your application?',
+        default: 'MEAN stack using typescript and Angular 2'
+      },
+      {
+        type: 'input',
+        name: 'version',
+        message: 'What version would you give your application?',
+        default: '0.1.0'
+      },
+      {
+        type: 'input',
+        name: 'keywords',
+        message: 'What keywords would you give your application?'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Who is your application author?',
+
+      }
+    ];
 
     this.prompt(prompts, function (props) {
       this.props = props;
@@ -27,11 +51,13 @@ module.exports = yeoman.generators.Base.extend({
     }.bind(this));
   },
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+  writing: {
+    createApplication: function () {
+      // TODO: Pull application from ng2-boilerplate submodule and write to targetDir
+    },
+    setPackageProperties: function () {
+      // TODO: Extend package.json with this.props
+    }
   },
 
   install: function () {
